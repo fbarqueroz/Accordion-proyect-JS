0e78644a4742a59a1355b814389098bbc2b9eb48
fbarqueroz <fabianbarquero9@gmail.com>
2020-10-05 13:36:01 -0600

    Cambios generales HTML

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 //
 // Script code (Accordion)
 //
+
+document.querySelectorAll('accorduion_button').forEach (button => {
+    button.addEventListener('click', () => {
+        const accordionContent = button.nextElementSibling; // nextElementSibling => Obtiene el contenido del HTML del siguiente elemento hermano.
+        button.classList.toggle('accordion')
+    })
+})
